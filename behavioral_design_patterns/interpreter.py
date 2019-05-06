d3c73fa077dbaaff45ea10d5a67c178378ffeaae
Anthony Torres <anthony_torres01@yahoo.com>
2019-05-05 22:13:15 -0500

    Add second part of interpreter pattern

--- behavioral_design_patterns/interpreter.py
+++ behavioral_design_patterns/interpreter.py
@@ -17,3 +17,33 @@ class AbstractExpression(metaclass=abc.ABCMeta):
     def interpret(self):
         pass
 
+
+class NonterminalExpression(AbstractExpression):
+    """
+    Implement an Interpret operation for nonterminal symbols in the grammar.
+    """
+
+    def __init__(self, expression):
+        self._expression = expression
+
+    def interpret(self):
+        self._expression.interpret()
+
+
+class TerminalExpression(AbstractExpression):
+    """
+    Implement an Interpret operation associated with terminal symbols in
+    the grammar.
+    """
+
+    def interpret(self):
+        pass
+
+
+def main():
+    abstract_syntax_tree = NonterminalExpression(TerminalExpression())
+    abstract_syntax_tree.interpret()
+
+
+if __name__ == "__main__":
+    main()
