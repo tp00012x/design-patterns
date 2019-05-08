05f46576f0f9826b915316c3c0d8f9ac3d31b11f
Anthony Torres <anthony_torres01@yahoo.com>
2019-05-08 10:29:26 -0500

    Add queue python example

---
+++ queue.py
@@ -0,0 +1,31 @@
+class Queue(object):
+    def __init__(self):
+        self.queue = []
+
+    def dequeue(self):
+        if len(self.queue) < 1:
+            return False
+        self.queue.pop(0)
+
+    def enqueue(self, item):
+        self.queue.append(item)
+
+    @property
+    def size(self):
+        return len(self.queue)
+
+
+example = Queue()
+
+example.enqueue(5)
+example.enqueue(2)
+example.enqueue(3)
+example.enqueue(1)
+
+print(example.queue)
+print(example.size)
+example.dequeue()
+example.dequeue()
+
+print(example.queue)
+print(example.size)
